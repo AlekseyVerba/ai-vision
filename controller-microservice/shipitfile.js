d1d58cf1ef0c8c3bdc6b658f06b2f41561f8fe1f
AlekseyVerba <leha.verba.01@mail.ru>
2023-02-24 17:05:08 +0300

    test

--- controller-microservice/shipitfile.js
+++ controller-microservice/shipitfile.js
@@ -80,9 +80,9 @@ module.exports = shipit => {
 
 
         shipit.blTask('npm-install', async () => {
-            await shipit.remote(`cd ${shipit.releasePath}/database-microservice && npm install --production`),
-            await shipit.remote(`cd ${shipit.releasePath}/controller-microservice && npm install --production`),
-            await shipit.remote(`cd ${shipit.releasePath}/mailer-microservice && npm install --production`)
+            await shipit.remote(`cd ${shipit.releasePath}/database-microservice && npm install`),
+            await shipit.remote(`cd ${shipit.releasePath}/controller-microservice && npm install`),
+            await shipit.remote(`cd ${shipit.releasePath}/mailer-microservice && npm install`)
           });
   
           shipit.blTask('build', async () => {
