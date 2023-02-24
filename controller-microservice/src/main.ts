cd2dc48b390fa98d629107457d2287c33ced649f
AlekseyVerba <leha.verba.01@mail.ru>
2023-02-24 18:28:28 +0300

    test

--- controller-microservice/src/main.ts
+++ controller-microservice/src/main.ts
@@ -3,6 +3,7 @@ import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/all-exception.filter';
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,11 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter)
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   app.enableCors();
+
+  console.log(process.cwd())
+  console.log('---------------------')
+
+  console.log(join(process.cwd(), '..', 'assets'))
 
   await app.listen(3000);
 }
