a91fb83ad920c80b8c4605db673f30544bd07a3d
AlekseyVerba <leha.verba.01@mail.ru>
2023-02-26 21:50:39 +0300

    test

--- controller-microservice/src/main.ts
+++ controller-microservice/src/main.ts
@@ -5,23 +5,31 @@ import { HttpExceptionFilter } from './filters/all-exception.filter';
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 import { join } from 'path';
 import { readFileSync } from 'fs'
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as http from 'http';
+import * as https from 'https';
 
 async function bootstrap() {
   const httpsOptions = {
     key: readFileSync(join(__dirname, '..', 'ssl', 'private.key')),
-    cert: readFileSync(join(__dirname, '..', 'ssl', 'certificate.pem')),
+    cert: readFileSync(join(__dirname, '..', 'ssl', 'certificate.crt')),
   };
 
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const server = express();
+
+  
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalFilters(new HttpExceptionFilter)
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   app.enableCors();
 
-  console.log(join(__dirname, '..', 'ssl', 'private.key'))
+  await app.init();
 
-  await app.listen(80);
+  http.createServer(server).listen(3000);
+  https.createServer(httpsOptions, server).listen(80);
   
 }
 bootstrap();
