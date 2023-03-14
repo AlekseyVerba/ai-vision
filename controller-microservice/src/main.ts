98280b57571b7859e0c457e5b44be6bed1480294
AlekseyVerba <leha.verba.01@mail.ru>
2023-03-14 11:39:54 +0300

    add https

--- controller-microservice/src/main.ts
+++ controller-microservice/src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   await app.init();
 
   http.createServer(server).listen(3000);
-  // https.createServer(httpsOptions, server).listen(443);
+  https.createServer(httpsOptions, server).listen(443);
 }
 bootstrap();
